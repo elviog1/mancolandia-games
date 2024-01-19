217db022e0666434b04abeaec1071d4db73d18d9
Elvio Galeano <elvio.galeano827@gmail.com>
2024-01-19 20:26:15 -0300

    mongodb

---
+++ app/lib/mongoose.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+let isConnected = false
+
+export const connectToDB = async ()=>{
+   mongoose.set('strickQuery',true)
+   
+   if(isConnected){
+    console.log("MongoDB is already connected")
+   }
+   try {
+    await mongoose.connect(process.env.MONGODB_URL,{
+        dbName: "Mongolandia",
+        useNewUrlParser:true,
+        useUnifiedTopology: true
+    });
+    isConnected= true
+
+    console.log("MongoDB is connected")
+   } catch (error) {
+    console.log(error)
+   }
+}
