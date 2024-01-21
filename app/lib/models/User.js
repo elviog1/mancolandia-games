4657e39c68ad21536681a52572cef9bc14e157d2
Elvio <elvio.galeano@gmail.com>
2024-01-21 01:41:37 -0300

    mongodb

---
+++ app/lib/models/User.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  profilePhoto: {
+    type: String,
+    required: true,
+  },
+  posts: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Posts" }],
+    default: [],
+  },
+  savedPosts: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Posts" }],
+    default: [],
+  },
+  likedPosts: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Posts" }],
+    default: [],
+  },
+  followers: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    default: [],
+  },
+  followings: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+
+export default User;
--- app/lib/mongoose.js
+++ app/lib/mongoose.js
@@ -1,23 +1,23 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-let isConnected = false
+let isConnected = false;
 
-export const connectToDB = async ()=>{
-   mongoose.set('strickQuery',true)
-   
-   if(isConnected){
-    console.log("MongoDB is already connected")
-   }
-   try {
-    await mongoose.connect(process.env.MONGODB_URL,{
-        dbName: "Mongolandia",
-        useNewUrlParser:true,
-        useUnifiedTopology: true
+export const connectToDB = async () => {
+  mongoose.set("strickQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "Mongolandia",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-    isConnected= true
+    isConnected = true;
 
-    console.log("MongoDB is connected")
-   } catch (error) {
-    console.log(error)
-   }
-}
+    console.log("MongoDB is connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
